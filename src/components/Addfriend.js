7f37a795e18ca5cd445a992d717334fd852078bf
lekshmi12345 <lekshmisnair213@gmail>
2022-05-19 22:54:59 +0530

    commit2

--- src/components/Addfriend.js
+++ src/components/Addfriend.js
@@ -1,65 +1,63 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import Header from './Header'
-
+import React from 'react'
 
 const Addfriend = () => {
-    var [name,setName]=useState("")
-    var [friendName,setFname]=useState("")
-    var [friendNickName,setNicName]=useState("")
-    var [DescribeYourFrienden,setDes]=useState("")
-   
-    
-  
+  return (
+      <div>
+    <div>
+        var [name,setName]=useState("")
+    var [friendname,setFriend]=useState("")
+    var [friendNickName,setNick]=useState("")
+    var [DescribeYourFriend,setDescribe]=useState("")
+
     const subData=()=>{
-        const data={"name":name,"friendName":friendName,"friendNickName":friendNickName,"DescribeYourFriend":DescribeYourFrienden}
+        const data={"name":name,"friendname":friendname,"friendNickName":friendNickName,"DescribeYourFriend":DescribeYourFriend}
         console.log(data)
         axios.post("https://dummyapifriends.herokuapp.com/adddata",data).then(
             (response)=>{
                 console.log(response.data)
                 if(response.data.status=="success")
                 {
-                    alert("succesfully added")
+                    alert("SUCCESSFULLY ADDED")
                 }
                 else{
-                    alert("failed to add")
+                    alert("FAILED TO ADD")
                 }
             }
         )
-    }
+            }
   return (
     <div>
-        <Header/>
-         <div className="container">
+        <Navbar/>
+        <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
-                
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label  for="" className="form-label">Name</label>
-                    <input  onChange={(e)=>{setName(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <label for="" className="form-label">NAME:</label>
+                    <input onChange={(e)=>{setName(e.target.value)}} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" className="form-label">friendName</label>
-                    <input  onChange={(e)=>{setFname(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                    <label for="" className="form-label">FRIENDS NAME:</label>
+                    <input onChange={(e)=>{setFriend(e.target.value)}} type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" className="form-label">friendNickName</label>
-                    <input  onChange={(e)=>{setNicName(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">FRIENDS NICKNAME:</label>
+                    <input onChange={(e)=>{setNick(e.target.value)}} type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" className="form-label">DescribeYourFriend</label>
-                    <input  onChange={(e)=>{setDes(e.target.value)}} className="form-control" placeholder="" type="text"/>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">DESCRIBE YOUR FRIEND:</label>
+                    <input onChange={(e)=>{setDescribe(e.target.value)}} type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button onClick={subData}  className="btn btn-primary">Register</button>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={subData} className="btn btn-success">REGISTER</button>
                 </div>
-               
             </div>
         </div>
     </div>
 </div>
-
+    </div>
+    </div>
+  )
     </div>
   )
 }
--- src/components/Viewfriend.js
+++ src/components/Viewfriend.js
@@ -1,67 +1,66 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import Header from './Header'
+import React from 'react'
 
+const Viewfriend = () => {
+  return (
+    <>
+     var [friendlist,setFriend]=useState([])
+    var [loadStatus,setLoadstatus]=useState(true)
 
-const View = () => {
-    var [viewlist,setViewlist]=useState([])
-    var[loadstatus,setLoadstatus]=useState(true)
     axios.get("https://dummyapifriends.herokuapp.com/view").then(
         (response)=>{
-            console.log(response.data)
-            setViewlist(response.data)
-            setLoadstatus(false)
+        console.log(response.data)
+        setFriend(response.data)
+        setLoadstatus(false)
         }
     )
-    
   return (
     <div>
-       <Header/>
-         <div className="container">
+    <Navbar/>
+    <div className="container">
     <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div className="row g-3">
-            
-            
-  
-    {loadstatus ? <div class="spinner-border" role="status">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    {loadStatus ?<div class="spinner-border" role="status">
   <span class="visually-hidden">Loading...</span>
-</div> : <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">name</th>
-      <th scope="col">friendName</th>
-      <th scope="col">friendNickName</th>
-      <th scope="col">DescribeYourFriend</th>
-  
-     
-    </tr>
-  </thead>
-   
-    <tbody>
-    {viewlist.map((value,key)=>{
-                    return  <tr>
-      <th scope="row">{value.name}</th>
-      <td>{value.friendName}</td>
-      <td>{value.friendNickName}</td>
-      <td>{value.DescribeYourFriend}</td>
-      
-    </tr>
+</div> :<table class="table table-primary table-striped">
+<thead>
+<tr>
+  <th scope="col">#</th>
+  <th scope="col">NAME</th>
+  <th scope="col">NICKNAME</th>
+  <th scope="col">FRIENDS NICKNAME</th>
+  <th scope="col">DESCRIBE YOUR FRIEND</th>
+</tr>
+</thead>
+<tbody>
     
- 
-                   
-                    
-                })}
-                 </tbody>
-  </table>
-}
-            </div>
-        </div>
-    </div>
-</div>
+        {friendlist.map((value,key)=>{
+return <tr>
+  <th scope="row">1</th>
+  <td>{value.name}</td>
+  <td>{value.friendName}</td>
+  <td>{value.friendNickName}</td>
+  <td>{value.DescribeYourFriend}</td>
 
+</tr>
+
+        })}
+
+          </tbody>
+</table>}
+
+    </div>
+    </div>
+    </div>
     </div>
   )
-}
+  
+    
+    
+    
+    </>
+  )
 
-export default View
+  
+
+
+export default Viewfriend
